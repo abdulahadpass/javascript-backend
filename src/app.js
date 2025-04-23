a34d682bc5c7883eab931d9b53ca0e4d481ee565
Abdul Ahad <ahadtahir662@gmail.com>
2025-04-23 22:23:25 +0500

    controller and route define

--- src/app.js
+++ src/app.js
@@ -12,4 +12,11 @@ app.use(express.urlencoded({ limit: '16kb', extended: true }))
 app.use(express.static('public'))
 app.use(cookieParser())
 
+// routes import 
+
+import userRouter from './routes/user.route.js'
+
+app.use('/api/v1/users', userRouter)
+
+
 export { app }
